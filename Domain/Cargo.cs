d39920ba687a9321c67df3e90102a8e269a48a61
Kelve Alves Gomes <kelveag2017@gmail.com>
2025-09-16 20:32:02 -0300

    Delete Domain directory

--- Domain/Cargo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hortifruti.Domain;
-
-public class Cargo
-{
-    public int id {get; set;}
-    public string? nome {get; set;}
-    public int? funcionario_id {get; set;}
-}
--- Domain/Log_login.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Hortifruti.Domain;
-
-public class Log_login
-{
-    public int id {  get; set; }
-    public string usuario { get; set; }
-    public DateTime data_hora { get; set; }
-}
