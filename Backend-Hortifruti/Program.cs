e54f922d8c13987b0a74fc1cd7b374cabbec5c1c
Zetsus <110355970+Zetsus-coding@users.noreply.github.com>
2025-09-17 20:22:55 -0300

    Update Program.cs
    Remoção dos addScoped anteriores

--- Backend-Hortifruti/Program.cs
+++ Backend-Hortifruti/Program.cs
@@ -15,14 +15,8 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
 builder.Services.AddControllers();  
-<<<<<<< HEAD
-builder.Services.AddScoped<ICategoriaRepository, CategoriaRepository>();
-builder.Services.AddScoped<ICategoriaService, CategoriaService>();
-builder.Services.AddScoped<IFornecedorRepository, FornecedorRepository>();
-builder.Services.AddScoped<IFornecedorService, FornecedorService>();
-=======
 builder.Services.AddApplicationServices();
->>>>>>> 1b9b8a271f515c5cbb60542048f1db5eda7f994d
+
 
 var app = builder.Build();
 
